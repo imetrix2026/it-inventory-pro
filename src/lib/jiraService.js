3649afae0f22582cfbfe16ad470617a9d6dd52d1
imetrix2026 <imetrix2022@gmail.com>
2026-06-25 15:25:43 +0300

    Update jiraService.js

--- src/lib/jiraService.js
+++ src/lib/jiraService.js
@@ -1,15 +1,8 @@
-const JIRA_URL = 'https://getsupport.atlassian.net'
-const JIRA_EMAIL = 'tassos@metrixnet.com'
-const JIRA_TOKEN = ''
-
 export async function fetchJiraWorklogs(projectKey, dateFrom) {
   const res = await fetch('/api/jira-proxy', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      jira_url: JIRA_URL,
-      jira_email: JIRA_EMAIL,
-      jira_token: JIRA_TOKEN,
       project_key: projectKey,
       date_from: dateFrom,
     })
